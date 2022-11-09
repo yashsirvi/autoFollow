6f5f2cf5cf32a39e6ab93e15e3d12922c528eb48
Manideep Sridhara <manideep.sridhara@kgpian.iitkgp.ac.in>
2022-11-09 14:24:08 +0530

    update

--- raspi_setup/uninstall_ros_one_line.sh
+++ raspi_setup/uninstall_ros_one_line.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
 wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_uninstall_noetic.sh && chmod +x ./ros_uninstall_noetic.sh && ./ros_uninstall_noetic.sh
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install python3-roslaunch
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc

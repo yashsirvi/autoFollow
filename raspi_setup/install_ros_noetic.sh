2933a5da93cb15997ad8d90e63c0fee7ee7d84f7
Manideep Sridhara <manideep.sridhara@kgpian.iitkgp.ac.in>
2022-11-09 14:14:28 +0530

    update

--- raspi_setup/install_ros_noetic.sh
+++ raspi_setup/install_ros_noetic.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt update
-sudo apt install ros-noetic-ros-base
-source /opt/ros/noetic/setup.bash
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install python3-rosdep
-sudo rosdep init
-rosdep update
+wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh

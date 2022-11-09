3c74a1c4162d98771879b2edef95cb51de708298
Manideep Sridhara <manideep.sridhara@kgpian.iitkgp.ac.in>
2022-11-09 15:23:11 +0530

    update

--- raspi_setup/install_ros_noetic.sh
+++ raspi_setup/install_ros_noetic.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh
+# wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh
+sudo apt-get update
+sudu apt-get upgrade
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt-get install build-essential cmake
+sudo apt install ros-noetic-ros-base
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 sudo apt install python3-roslaunch
+source /opt/ros/noetic/setup.bash
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-roslaunch
+sudo groupadd -f --system gpio
+sudo usermod -a -G gpio pi
+sudo usermod -a -G i2c pi

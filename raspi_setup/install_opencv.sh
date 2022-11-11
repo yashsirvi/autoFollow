e6fa4f228e1894e76deca0dc8eb29b5d88298122
Manideep Sridhara <manideep.sridhara@kgpian.iitkgp.ac.in>
2022-11-11 19:06:42 +0530

    Add opencv_installation script

---
+++ raspi_setup/install_opencv.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+sudo apt install snapd
+sudo snap install cmake --classic
+sudo apt-get install python3-dev
+sudo apt install unzip
+sudo apt install python3-pip
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+pip install numpy
+cd ~/opencv-4.0.0
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-4.0.0/modules \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D WITH_TBB=OFF \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D BUILD_EXAMPLES=OFF ..
+make –j4
+sudo apt-get install libopencv-devpython-opencv
